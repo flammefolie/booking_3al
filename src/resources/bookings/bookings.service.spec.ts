11b727e062f9e7d4aed5b8556c1a3e8d589ef36b
melis <melissanoemi.laurent@free.fr>
2023-07-16 20:29:28 +0200

    test for crud booking

--- src/resources/bookings/bookings.service.spec.ts
+++ src/resources/bookings/bookings.service.spec.ts
@@ -6,6 +6,8 @@ import {User} from "../users/entities/user.entity";
 import {Room} from "../rooms/entities/room.entity";
 import {getRepositoryToken} from "@nestjs/typeorm";
 import {CreateBookingDto} from "./dto/create-booking.dto";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { UpdateBookingDto } from "./dto/update-booking.dto";
 
 const mockBookingsRepository = {
   save: jest.fn().mockImplementation((dto: CreateBookingDto) => {
@@ -148,5 +150,168 @@ describe('BookingsService', () => {
         await expect(service.create(createBookingDto, "userId")).toThrow(RoomAlreadyBookedAtThisTimeException);
       }
   );
+
+
+  describe("findAll", () => {
+    it("should return an array of bookings", async () => {
+
+      const booking1 = new Booking();
+      booking1.id = "booking-id-1";
+      const booking2 = new Booking();
+      booking2.id = "booking-id-2";
+
+      const bookings = [booking1, booking2];
+
+      jest.spyOn(bookingRepository, "find").mockResolvedValue(bookings as any);
+      const result = await service.findAll();
+
+      // Vérification que la méthode `find()` a été appelée
+      expect(bookingRepository.find).toHaveBeenCalled();
+
+      // Vérification que le résultat correspond aux réservations factices
+      expect(result.length).toEqual(bookings.length);
+      expect(result).toEqual(bookings);
+
+    });
+
+  });
+
+  describe("findOne", () => {
+    it("should return a booking by id", async () => {
+
+      const booking1 = new Booking();
+      booking1.id = "booking-id-1";
+
+      jest.spyOn(bookingRepository, "findOne").mockResolvedValue(booking1);
+      const result = await service.findOne(booking1.id);
+
+      // Vérification que la méthode `find()` a été appelée
+      expect(bookingRepository.findOne).toHaveBeenCalled();
+
+      // Vérification que le résultat correspond aux réservations factices
+      expect(result).toEqual(booking1);
+      expect(result.id).toEqual(booking1.id);
+
+    });
+
+  });
+
+  describe("remove", () => {
+    let booking: Booking;
+    let user: User;
+
+    beforeEach(() => {
+      booking = new Booking();
+      booking.id = "booking-id";
+
+      user = new User();
+      user.id = "user id";
+      user.userName = "Test User";
+      booking.user = user;
+    });
+
+
+    it("it should throw an exception because the user is unauthorised to remove a booking", async () => {
+      jest.spyOn(bookingRepository, "findOne").mockResolvedValue(booking);
+
+      const userId = "another-user-id";
+
+      await expect(async () => {
+        await service.remove(booking.id, userId);
+      }).rejects.toThrow(HttpException);
+
+      try {
+        await service.remove(booking.id, userId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(HttpException);
+        expect(error.getStatus()).toBe(HttpStatus.FORBIDDEN);
+        expect(error.getResponse()).toEqual({
+          status: HttpStatus.FORBIDDEN,
+          error: "You are not authorize to delete this resource"
+        });
+      }
+    });
+
+
+    it("it should remove a booking if the user is authorised", async () => {
+      jest.spyOn(bookingRepository, "findOne").mockResolvedValue(booking);
+      jest.spyOn(bookingRepository, "delete").mockResolvedValue(undefined);
+
+      await service.remove(booking.id, user.id);
+      expect(bookingRepository.findOne).toHaveBeenCalled();
+      expect(bookingRepository.delete).toHaveBeenCalled();
+    });
+  });
+
+
+  describe("update", () => {
+    let booking: Booking;
+    let user: User;
+    let room: Room;
+    let updateBookingDto: UpdateBookingDto;
+
+    beforeEach(() => {
+      booking = new Booking();
+      booking.id = "booking-id";
+
+      user = new User();
+      user.id = "user id";
+      user.userName = "Test User";
+      booking.user = user;
+
+      room = new Room();
+      room.id = "room id";
+      room.name = "Test Room";
+      room.description = "This is a test room";
+
+      updateBookingDto = new UpdateBookingDto();
+      updateBookingDto.startTime = new Date().setTime(10).toString();
+      updateBookingDto.endTime = new Date().setTime(11).toString();
+      updateBookingDto.room = room.id;
+
+    });
+
+
+    it("it should throw an exception because the user is unauthorised to update a booking", async () => {
+      jest.spyOn(bookingRepository, "findOne").mockResolvedValue(booking);
+
+      const userId = "another-user-id";
+
+      await expect(async () => {
+        await service.update(booking.id, updateBookingDto, userId);
+      }).rejects.toThrow(HttpException);
+
+      try {
+        await service.update(booking.id, updateBookingDto, userId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(HttpException);
+        expect(error.getStatus()).toBe(HttpStatus.FORBIDDEN);
+        expect(error.getResponse()).toEqual({
+          status: HttpStatus.FORBIDDEN,
+          error: "You are not authorize to update this resource"
+        });
+      }
+    });
+
+
+    it("it should update a booking if the user is authorised", async () => {
+      jest.spyOn(roomRepository, "findOne").mockResolvedValue(room);
+      jest.spyOn(bookingRepository, "findOne").mockResolvedValue(booking);
+      jest.spyOn(bookingRepository, "save").mockResolvedValue(undefined);
+
+      await service.update(booking.id, updateBookingDto, user.id);
+
+      expect(bookingRepository.findOne).toHaveBeenCalledWith({
+        where: { id: booking.id },
+        relations: ["room", "user"]
+      });
+
+      expect(roomRepository.findOne).toHaveBeenCalledWith({
+        where: { id: updateBookingDto.room }
+      });
+
+      expect(bookingRepository.save).toHaveBeenCalledWith(booking);
+    });
+  });
 
 });
